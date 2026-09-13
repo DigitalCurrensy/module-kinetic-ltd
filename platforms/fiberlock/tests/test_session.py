e9248624d4997f6bed2698ac11fcfcf714d43f11
Digital Currensy Inc. <38707648+DigitalCurrensy@users.noreply.github.com>
2026-09-13 00:24:21 -0700

    C37.238 / BMCA timeout note; wrap_from_pin tests.

--- platforms/fiberlock/tests/test_session.py
+++ platforms/fiberlock/tests/test_session.py
@@ -1,4 +1,9 @@
-from platforms.fiberlock.src.application.session import WrapRefused, wrap_control_path
+from platforms.fiberlock.src.application.session import WrapRefused, wrap_control_path, wrap_from_pin
+
+
+class _Pin:
+    def __init__(self, time_source):
+        self.time_source = time_source
 
 
 def test_wrap_under_qber():
@@ -12,3 +17,16 @@ def test_refuse_high_qber():
         assert False
     except WrapRefused as exc:
         assert exc.http_status == 422
+
+
+def test_wrap_from_csac_pin():
+    session = wrap_from_pin("span-4", "ks-3", 0.04, 256, _Pin("csac"))
+    assert session.wrapped is True
+
+
+def test_refuse_gps_peer_pin():
+    try:
+        wrap_from_pin("span-4", "ks-4", 0.04, 256, _Pin("gps_peer"))
+        assert False
+    except WrapRefused:
+        pass
